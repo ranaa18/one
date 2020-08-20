f4872794903857e6bc9af033568f6f5aacaedae2
rana <rana.atito18@gmail.com>
2020-08-20 05:43:11 +0200

    first commit

--- 1.js
+++ 1.js
@@ -1,6 +1,9 @@
 $(function (){
 
     'use strict';
+
+    //calculate body padding top // navbar hight
+    $('body').css('paddingTop', $('.navbar').innerHeight());
     //smoothy scroll to element
 
     $('.navbar li a').click(function (e){
