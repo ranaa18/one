f07541524884802b33a4032f9a6f45c09f20d53e
rana <rana.atito18@gmail.com>
2020-08-21 03:15:39 +0200

    after edit index.html

--- 1.js
+++ 1.js
@@ -4,6 +4,17 @@ $(function (){
 
     //calculate body padding top // navbar hight
     $('body').css('paddingTop', $('.navbar').innerHeight());
+
+    // add active class on navbar link 
+    $('.navbar li a').click(function(){
+
+        $('.navbar li a').removeClass('active');
+
+        $(this).addClass('active');
+
+       // $(this).addClass('active').parent().siblings().find('a').removeClass('active');
+    });
+
     //smoothy scroll to element
 
     $('.navbar li a').click(function (e){
@@ -14,8 +25,9 @@ $(function (){
 
             scrollTop: $('#' + $(this).data('scroll')).offset().top
         }, 1000);
-            
 
     });
+    
+    
     
 });
